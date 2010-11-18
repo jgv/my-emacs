c0df96f90513d09aa8fe0c656332944e864bdc1d
jgv <jvingiano@gmail.com>
2010-11-18 16:21:03 -0500

    Inital commit

---
+++ customizations/my-ui.el
@@ -0,0 +1,34 @@
+(setq inhibit-startup-message t) ; no startup message
+
+(set-variable 'truncate-lines t) ; truncate whitespace
+
+(transient-mark-mode 1) 
+
+(setq query-replace-highlight t) ;; highlight in search and replace
+
+(setq search-highlight t) ;; highlight searches
+
+(setq font-lock-maximum-decoration t)
+
+(setq display-time-day-and-date t) (display-time)
+
+(setq scroll-conservatively 10000
+      scroll-step 1) ; smooth scroll
+
+(setq visible-bell 1)
+
+(setq ring-bell-function 'ignore) ;; no bell
+
+(set-scroll-bar-mode -1) ;; no scrollbar
+
+(menu-bar-mode 1) ;; no menu
+
+(tool-bar-mode 0) ;; no toolbar
+
+(global-hl-line-mode 1)
+
+(require 'color-theme) ; give it some color
+(color-theme-initialize)
+(color-theme-clarity)
+
+(set-face-background 'hl-line "#1A1A1A")
---
+++ init.el
@@ -0,0 +1,11 @@
+; enable Common Lisp support
+(require 'cl)
+
+; directories for the load path
+(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/customizations")
+(add-to-list 'load-path "~/.emacs.d/util")
+(add-to-list 'load-path "~/.emacs.d/vendor")
+
+; load some modes
+(vendor 'color-theme)
