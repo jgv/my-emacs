03edb4f5ce173fa47a7688de18819304822f85f0
jgv <jvingiano@gmail.com>
2010-11-18 17:09:01 -0500

    elpa

--- init.el
+++ init.el
@@ -1,6 +1,17 @@
 ; enable Common Lisp support
 (require 'cl)
 
+;;; This was installed by package-install.el.                                                                                                             
+;;; This provides support for the package system and                                                                                                     
+;;; interfacing with ELPA, the package archive.                                                                                                           
+;;; Move this code earlier if you want to reference                                                                                                      
+;;; packages in your .emacs.                                                                                                                              
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
+
+
 ; directories for the load path
 (add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/customizations")
@@ -15,3 +26,12 @@
 (vendor 'centered-cursor-mode)
 
 (mapcar 'load-directory '("~/.emacs.d/customizations"))
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
