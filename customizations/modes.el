6451b519d886704ad8c76d47833444e1721568d3
jgv <jvingiano@gmail.com>
2011-02-14 17:02:55 -0500

    adding gmail stuff for gnus

--- customizations/modes.el
+++ customizations/modes.el
@@ -71,5 +71,10 @@
     (text-mode . dabbrev-completion)))
 
 ; tweets
-
 (require 'twittering-mode)
+
+; gmail
+(add-to-list 'gnus-secondary-select-methods '(nnimap "gmail"
+                                  (nnimap-address "imap.gmail.com")
+                                  (nnimap-server-port 993)
+                                  (nnimap-stream ssl)))
