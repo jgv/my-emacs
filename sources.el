8bc85be91de41906aebb365dbb649faca251c9bf
Jonathan Vingiano <jvingiano@gmail.com>
2012-11-01 17:37:50 -0400

    switch from lambda to progn

--- sources.el
+++ sources.el
@@ -2,103 +2,97 @@
       '((:name ruby-mode 
                :type elpa
                :load "ruby-mode.el"
-               :after (lambda () (ruby-mode-hook)))
+               :after (progn (ruby-mode-hook)))
         (:name inf-ruby :type elpa)
         (:name ruby-compilation :type elpa)
         (:name php-mode 
                :type git
                :url "git://github.com/ejmr/php-mode.git"
                :features php-mode
-               :after (lambda () (php-mode-hook)))
+               :after (progn (php-mode-hook)))
         (:name css-mode 
                :type elpa
-               :after (lambda () (css-mode-hook)))
+               :after (progn (css-mode-hook)))
         (:name textmate
                :type git
                :url "git://github.com/defunkt/textmate.el"
                :load "textmate.el"
-               :after (lambda () (textmate-hook)))
+               :after (progn (textmate-hook)))
         (:name magit 
-               :after (lambda () (magit-hook)))
-        (:name rvm
-               :type git
-               :url "git://github.com/djwhitt/rvm.el.git"
-               :load "rvm.el"
-               :compile ("rvm.el")
-               :after (lambda() (rvm-use-default)))
+               :after (progn (magit-hook)))
         (:name yaml-mode
                :type git
                :url "git://github.com/yoshiki/yaml-mode.git"
                :features yaml-mode
-               :after (lambda () (yaml-mode-hook)))
+               :after (progn (yaml-mode-hook)))
         (:name rhtml
                :type git
                :url "git://github.com/eschulte/rhtml.git"
                :features rhtml-mode
-               :after (lambda () (rhtml-mode-hook)))
+               :after (progn (rhtml-mode-hook)))
         (:name centered-cursor-mode
                :type git
                :url "git://github.com/emacsmirror/centered-cursor-mode.git"
                :load "centered-cursor-mode.el"
-               :after (lambda () (centered-cursor-hook)))
+               :after (progn (centered-cursor-hook)))
         (:name rainbow-mode
                :type git
                :url "git://github.com/emacsmirror/rainbow-mode.git"
                :features rainbow-mode
-               :after (lambda () (rainbow-mode-hook)))
+               :after (progn (rainbow-mode-hook)))
         (:name smart-tab
                :type git
                :url "git://github.com/genehack/smart-tab.git"
                :load "smart-tab.el"
-               :after (lambda () (smart-tab-hook)))
+               :after (progn (smart-tab-hook)))
         (:name textile-mode
                :type git
                :url "git://github.com/emacsmirror/textile-mode.git"
                :features textile-mode
-               :after (lambda () (textile-mode-hook)))
+               :after (progn (textile-mode-hook)))
         (:name markdown-mode
                :type git
                :url "git://jblevins.org/git/markdown-mode.git"
                :features markdown-mode
-               :after (lambda () (markdown-mode-hook)))
+               :after (progn (markdown-mode-hook)))
         (:name coffee-mode
                :type git
                :url "git://github.com/defunkt/coffee-mode.git"
                :features coffee-mode
-               :after (lambda () (coffee-mode-hook)))
+               :after (progn (coffee-mode-hook)))
         (:name yasnippet
                :type git
                :url "git://github.com/capitaomorte/yasnippet.git"
                :features yasnippet
-               :after (lambda () (yasnippet-hook)))
+               :after (progn (yasnippet-hook)))
         (:name io-mode
                :type git
                :url "git://github.com/superbobry/io-mode.git"
                :features io-mode
-               :after (lambda () (io-mode-hook)))
+               :after (progn (io-mode-hook)))
         (:name io-moode-inf
                :type git
                :url "git://github.com/slackorama/io-emacs.git"
                :features io-mode-inf
-               :after (lambda () (io-mode-inf-hook)))
+               :after (progn (io-mode-inf-hook)))
         (:name ethan-wspace
-               :after (lambda () (global-ethan-wspace-mode 1)))
+               :after (progn (global-ethan-wspace-mode 1)))
         (:name js-comint
                :type git
                :url "git://github.com/user-1/js-comint.git"
-               :after (lambda () (js-comint-hook)))
+               :after (progn (js-comint-hook)))
         (:name tree-mode ;; dependency of dir-tree
                :type git
                :url "git://github.com/emacsmirror/tree-mode.git"
-               :after (lambda() (require 'tree-mode)))
+               :after (progn (require 'tree-mode)))
         (:name windata
                :type git
                :url "git://github.com/emacsmirror/windata.git"
-               :after (lambda() (require 'windata)))
+               :after (progn (require 'windata)))
         (:name dir-tree
                :type git
                :url "git://github.com/zkim/emacs-dirtree.git"
-               :after (lambda () (require 'dirtree)))))
+               :after (progn (require 'dirtree)))))
 
 (setq my-packages
       (append '(el-get) (mapcar 'el-get-source-name el-get-sources)))
